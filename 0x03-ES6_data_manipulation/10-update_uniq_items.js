9c38d974f18a412732b61b62ab98b0162e158166
kZilla <kzilla@vn-z.zilla>
2024-07-29 13:46:56 +0300

    ES6 - data manipulation

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,8 @@
+export default function updateUniqItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+  return map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
