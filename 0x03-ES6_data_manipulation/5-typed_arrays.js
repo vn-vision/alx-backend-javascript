474de349fda59bbc22ba5584127a7e07d4cfabb5
kZilla <kzilla@vn-z.zilla>
2024-07-30 10:28:26 +0300

    ES6 - data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
   if (position >= length) {
     throw Error('Position outside range');
   } else {
-    int8View[position] = value;
-    return int8View;
+    view.setInt8(position, value);
+    return view;
   }
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,22 @@
+// This code implements a function that takes a set of strings and a string as arguments.
+// It returns a string that contains all the strings from the set that start with the given string.
+// The strings are separated by a hyphen with the given string removed from the start of each string
+
+// export default function cleanSet(set, startString) {
+//   if (typeof startString !== 'string' || !startString) return '';
+//   const res = [...set].reduce((string, currentWord) => {
+//     if (currentWord.startsWith(startString)) {
+//       return string + (string ? '-' : '') + currentWord.slice(startString.length);
+//     }
+//     return string;
+//   }, '');
+//   return res;
+// }
+
+// this is an alternative solution, the first one is more efficient.
+// this is more readable and easier to understand.
 export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || !startString) return '';
-  const res = [...set].reduce((string, currentWord) => {
-    if (currentWord.startsWith(startString)) {
-      return string + (string ? '-' : '') + currentWord.slice(startString.length);
-    }
-    return string;
-  }, '');
-  return res;
+  const res = [...set].filter((word) => word.startsWith(startString));
+  return res.map((word) => word.slice(startString.length)).join('-');
 }
